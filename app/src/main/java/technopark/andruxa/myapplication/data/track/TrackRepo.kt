1e4b76b2717a871f1b6000710daaf128037c5a0f
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-11-28 23:18:27 +0300

    track repo interface improved

---
+++ app/src/main/java/technopark/andruxa/myapplication/data/track/TrackRepo.kt
@@ -0,0 +1,18 @@
+package technopark.andruxa.myapplication.data.track
+
+import technopark.andruxa.myapplication.models.Album
+
+interface TrackRepo {
+    fun getByMbid(
+        mbid: String,
+        autoCorrect: Boolean?,
+        userName: String?,
+    ): Album
+
+    fun getByNameNArtist(
+        name: String,
+        artistName: String,
+        autoCorrect: Boolean?,
+        userName: String?,
+    ): Album
+}
