5b7b69f1ba3e8ae327f2fe25a91e7e8e92155219
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-11-29 16:21:27 +0300

    great commit

---
+++ app/src/main/java/technopark/andruxa/myapplication/data/additional/room/album/AlbumRoomEntity.kt
@@ -0,0 +1,28 @@
+package technopark.andruxa.myapplication.data.additional.room.album
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import technopark.andruxa.myapplication.models.album.Album
+import technopark.andruxa.myapplication.models.tag.Tag
+import technopark.andruxa.myapplication.models.track.Track
+import java.util.*
+
+@Entity
+class AlbumRoomEntity: Album {
+    @ColumnInfo override var name: String = ""
+    @ColumnInfo override var artistName: String = ""
+    @PrimaryKey override var id: Int = -1
+    @ColumnInfo override var mbid: String = ""
+    @ColumnInfo override var url: String = "" 
+    @ColumnInfo override var release: String = ""
+    @ColumnInfo override var imageSmallUrl: String = ""
+    @ColumnInfo override var imageMediumUrl: String = ""
+    @ColumnInfo override var imageLargeUrl: String = ""
+    @ColumnInfo override var listenerNum: Int = 0
+    @ColumnInfo override var playCount: Int = 0
+    @ColumnInfo override var topTags: List<Tag>
+    @ColumnInfo override var tracks: List<Track>
+    @ColumnInfo override var errorCode: Int = 0
+    @ColumnInfo override var message: String = ""
+}
