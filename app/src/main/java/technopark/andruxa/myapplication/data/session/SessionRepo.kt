8367e68f96a073246cd569f02a1ee707ccba7c41
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-12-21 14:34:48 +0300

    auth improved

--- app/src/main/java/technopark/andruxa/myapplication/data/session/SessionRepo.kt
+++ app/src/main/java/technopark/andruxa/myapplication/data/session/SessionRepo.kt
@@ -22,15 +22,36 @@ class SessionRepo: ISessionRepo {
     private val lastFmStore = LastFmStore.instance.sessionApi
 
     override fun login(login: String, password: String): SDataI<Boolean> {
-        lastFmStore.login(login, password, Constants.lastFmApiKey,
-                Utils.md5("api_key" + Constants.lastFmApiKey + "methodauth.getMobileSessionpassword" + password + "username" + login + Constants.lastFmApiSecret)
+        lastFmStore.login(
+            login,
+            password,
+            Utils.md5("api_key" + Constants.lastFmApiKey + "methodauth.getMobileSessionpassword" + password + "username" + login + Constants.lastFmApiSecret)
         ).enqueue(
             object : Callback<SessionResponseXML> {
                 override fun onResponse(
                     call: Call<SessionResponseXML>,
                     response: Response<SessionResponseXML>
                 ) {
+                    Log.d("auth", "started!")
                     response.body()?.let {
+                        if (it.status != null) {
+                            Log.d("auth resp status", it.status!!)
+                        } else {
+                            Log.d("auth resp status", "NO STATUS")
+                        }
+
+                        if (it.sessionKey != null) {
+                            Log.d("auth resp session key", it.sessionKey!!)
+                        } else {
+                            Log.d("auth resp session key", "NO SKEY")
+                        }
+
+                        if (it.apiSig != null) {
+                            Log.d("auth resp api sig", it.apiSig!!)
+                        } else {
+                            Log.d("auth resp api sig", "NO ASIG")
+                        }
+
                         Log.d("auth", "got session key")
                         isLogined.setData(it.sessionKey != null)
                         sessionKey = it.sessionKey
--- app/src/main/java/technopark/andruxa/myapplication/data/storages/lastFm/session/SessionRequester.kt
+++ app/src/main/java/technopark/andruxa/myapplication/data/storages/lastFm/session/SessionRequester.kt
@@ -3,13 +3,14 @@ package technopark.andruxa.myapplication.data.storages.lastFm.session
 import retrofit2.Call
 import retrofit2.http.POST
 import retrofit2.http.Query
+import technopark.andruxa.myapplication.data.storages.lastFm.LastFmStore
 
 interface SessionRequester {
     @POST("2.0?method=auth.getMobileSession")
     fun login(
-            @Query("username") username: String,
-            @Query("password") password: String,
-            @Query("api_key") api_key: String,
-            @Query("api_sig") api_sig: String
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("api_sig") api_sig: String,
+        @Query("api_key") api_key: String = LastFmStore.instance.apiKey,
     ): Call<SessionResponseXML>
 }
